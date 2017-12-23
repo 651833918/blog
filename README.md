479fe6bbaa23454d8645e28d3934a22ef8aff7ed
Xue <651833918@qq.com>
2017-12-23 21:47:51 +0800

    READEME

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blog
-个人博客 --用spring,springmvc,mybatis等框架写的个人博客（尚未完成）
+个人博客 --SSM+Velocity+editor.md实现的个人博客，对接了七牛云进行了图片存储（已完成）
+包括mysql数据库的sql文件
--- blog.sql
+++ blog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-11-27 22:44:11
+Date: 2017-12-23 00:15:49
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,21 +20,19 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
-  `id` int(50) NOT NULL,
+  `id` int(50) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) DEFAULT NULL,
-  `content` text,
+  `content` mediumtext,
   `title` varchar(255) DEFAULT NULL,
-  `likenum` int(10) DEFAULT NULL COMMENT '点赞数',
-  `looknum` int(12) DEFAULT NULL COMMENT '浏览量',
+  `likenum` int(10) DEFAULT '0' COMMENT '点赞数',
+  `looknum` int(12) DEFAULT '0' COMMENT '浏览量',
   `time` varchar(50) DEFAULT NULL COMMENT '发布时间',
   `tag_id` int(10) DEFAULT NULL COMMENT '标签主键',
-  `img_url` varchar(50) DEFAULT NULL COMMENT '文章内容图片',
+  `img_url` mediumtext COMMENT '文章内容图片',
+  `commentnum` int(12) DEFAULT '0',
+  `state` int(2) DEFAULT NULL COMMENT '0 草稿状态 1发表状态  默认为发表',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of article
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=186 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for blog
@@ -42,49 +40,37 @@ CREATE TABLE `article` (
 DROP TABLE IF EXISTS `blog`;
 CREATE TABLE `blog` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '博客主键',
-  `blog_name` varchar(20) NOT NULL COMMENT '博客名',
-  `blog_intro` varchar(50) NOT NULL COMMENT '博客简介',
-  `blog_url` varchar(20) DEFAULT NULL COMMENT '博客地址',
+  `blog_name` varchar(20) DEFAULT NULL COMMENT '博客名',
+  `blog_intro` varchar(50) DEFAULT NULL COMMENT '博客简介',
+  `blog_url` varchar(255) DEFAULT NULL COMMENT '博客地址',
   `user_id` int(10) NOT NULL COMMENT '用户主键',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of blog
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for blogroll
 -- ----------------------------
 DROP TABLE IF EXISTS `blogroll`;
 CREATE TABLE `blogroll` (
-  `id` int(20) NOT NULL,
+  `blogroll_id` int(20) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL COMMENT '用户主键',
   `describe` varchar(255) DEFAULT NULL COMMENT '描述',
-  `url` varchar(20) DEFAULT NULL COMMENT '地址',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of blogroll
--- ----------------------------
+  `url` varchar(50) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`blogroll_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for comment
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
-  `id` int(100) NOT NULL,
+  `comment_id` int(100) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) DEFAULT NULL,
   `article_id` int(50) DEFAULT NULL,
   `comment_content` varchar(255) DEFAULT NULL,
   `commont_time` varchar(50) DEFAULT NULL COMMENT '评论时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of comment
--- ----------------------------
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for keyword
@@ -97,42 +83,30 @@ CREATE TABLE `keyword` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of keyword
--- ----------------------------
-
--- ----------------------------
 -- Table structure for tag
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag` (
-  `id` int(10) NOT NULL,
+  `tag_id` int(10) NOT NULL AUTO_INCREMENT,
   `tag_name` varchar(20) DEFAULT NULL COMMENT '标签名',
   `user_id` int(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tag
--- ----------------------------
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户主键',
+  `user_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户主键',
   `username` varchar(50) NOT NULL COMMENT '用户名（登录）',
   `password` varchar(50) NOT NULL COMMENT '密码',
-  `nick_name` varchar(20) DEFAULT '' COMMENT '昵称',
+  `nick_name` varchar(20) DEFAULT NULL COMMENT '昵称',
   `email` varchar(50) DEFAULT '' COMMENT '邮箱',
   `sex` varchar(10) DEFAULT '男' COMMENT '性别',
   `head_img` varchar(255) DEFAULT NULL COMMENT '头像',
   `user_intro` varchar(255) DEFAULT NULL COMMENT '个人简介',
   `birthday` varchar(30) DEFAULT NULL,
-  `looknum` int(12) DEFAULT NULL COMMENT '浏览量',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user
--- ----------------------------
+  `looknum` int(12) DEFAULT '0' COMMENT '浏览量',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
